0095a782aec3b6bace746f0a29ecaf72f8015ccf
ShriKrishnaMahalAvp <shrikrishnamahalavp@gmail.com>
2026-03-02 20:28:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,12 @@
 
 const CONFIG = {
     // Gallery Config - Google Apps Script URL
-    galleryUrl: 'https://script.google.com/macros/s/AKfycby7edmZEdORBU1V0bSrEXVgburwsEyT0McVst3OZFxJOQbzVYu26DoU-38FPIVKdA3I/exec',
+    galleryUrl: 'https://script.google.com/macros/s/AKfycbzmigwTVX5fQCB_fUyjcErn4SlcGPMcxi-KFOr6AcIYMcVZg_190DUeloF9QewJdm8o/exec',
     
     // Calendar Config
     calendarTitle: "Availability Checker - Shri Krishna Mahal",
     // Google Calendar public ICS URL
     calendarIcsUrl: "https://calendar.google.com/calendar/ical/f7beb684c4719068f5e4886e5da04969ea34a1b266eebae19781b361b513bca9%40group.calendar.google.com/public/basic.ics"
 };
+
 
