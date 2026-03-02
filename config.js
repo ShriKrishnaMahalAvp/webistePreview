cd1b20f97190951f1e4d5ebc8ba45ef2fe53a2dd
ShriKrishnaMahalAvp <shrikrishnamahalavp@gmail.com>
2026-03-02 20:26:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,3 +10,4 @@ const CONFIG = {
     // Google Calendar public ICS URL
     calendarIcsUrl: "https://calendar.google.com/calendar/ical/f7beb684c4719068f5e4886e5da04969ea34a1b266eebae19781b361b513bca9%40group.calendar.google.com/public/basic.ics"
 };
+
